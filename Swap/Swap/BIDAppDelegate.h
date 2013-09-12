2e42002f490382ae2f468950da6a88f05fe22d91
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-12 13:42:33 +0800

    Created Swap Project

---
+++ Swap/Swap/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  Swap
+//
+//  Created by Maria Alice C. Lim on 9/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
